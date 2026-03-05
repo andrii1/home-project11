4f2a685a228866074bafa660788d89bee31dcae4
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2026-03-05 19:27:55 +0100

    wip

--- packages/server/migrations/20230217125345_add_categories_table.js
+++ packages/server/migrations/20230217125345_add_categories_table.js
@@ -6,7 +6,8 @@ exports.up = function (knex) {
   return knex.schema.createTable('categories', (table) => {
     table.increments();
     table.string('title').notNullable();
-    table.string('nodeId').nullable();
+    table.text('meta_description').nullable();
+    table.string('slug').notNullable();
   });
 };
 
--- packages/server/migrations/20230217125639_add_products_table.js
+++ packages/server/migrations/20230217125639_add_products_table.js
@@ -1,11 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable('products', (table) => {
     table.increments();
-    table.string('asin', 100).nullable();
+    table.string('platform_specific_id', 100).nullable();
     table.text('title').notNullable();
     table.string('slug').notNullable();
 
     table.text('description').nullable();
+    table.text('descriptionChatGpt').nullable();
 
     table.integer('category_id').unsigned();
     table.foreign('category_id').references('id').inTable('categories');
@@ -14,26 +15,16 @@ exports.up = function (knex) {
     table.text('url_affiliate').nullable();
     table.text('url_image').nullable();
     table.string('url_icon').nullable();
-    table.string('url_serpapi').nullable();
-    table.string('options').nullable();
-    table.string('brand').nullable();
     table.decimal('rating', 2, 1).unsigned().nullable();
     table.integer('reviews').unsigned().nullable();
     table.decimal('price', 10, 2).unsigned().nullable();
-    table.decimal('price_original', 10, 2).unsigned().nullable();
     table.integer('discount_percentage').unsigned().nullable();
-    table.string('currency', 10).defaultTo('USD');
-    table.boolean('is_prime').defaultTo(false);
+    table.string('currency', 10).defaultTo('EUR');
     table.boolean('featured').defaultTo(false);
     table.boolean('sponsored').defaultTo(false);
     table.boolean('top_rated').defaultTo(false);
-
     table.integer('bought_last_month').unsigned().nullable();
-    table.integer('rank').unsigned().nullable(); // Movers & Shakers / Best Sellers rank
-
-    table.string('source').nullable();
-
-    table.json('raw_data').nullable(); // Full SERP API dump
+    table.integer('rank').unsigned().nullable();
 
     table.string('meta_description').nullable();
 
